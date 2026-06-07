5afe472de49fe57cda2c79b15671dcfa4bf97ffd
RtekiX <443294734@qq.com>
2026-06-07 21:33:57 +0800

    Initial Commit

---
+++ Xdnmb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xdnmb
+//
+//  Created by rtekix on 2026/6/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
